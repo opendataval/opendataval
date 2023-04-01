b8251d62632505266c2bdafc2650c8227bdc4038
kevinfjiang <kevinfjiang@yahoo.com>
2023-04-01 01:15:10 -0400

    Out-of-Bag Data valuation implementation   1. Used a modified defaultdict to facilitate groupings of each oob     prediction. We track where each label is inserted in a larger     tensor so we can slice later   2. Allows user specified evaluation metric, however, you must   be careful the metric allows comparisons between each label   and the oob predictions for that label only. Values such as   F1 score or recall will not apply.

---
+++ dataoob/dataval/oob/oob.py
@@ -0,0 +1,133 @@
+import numpy as np
+import tqdm
+
+import torch
+
+from torch.utils.data import Subset
+import copy
+
+from dataoob.dataval import DataEvaluator
+from numpy.random import RandomState
+from sklearn.utils import check_random_state
+
+from collections import defaultdict
+
+
+class DataOob(DataEvaluator):
+    """Data Out-of-Bag data valuation implementation.
+    Input evaluation metrics are valid if we compare one data point across several
+    predictions. Examples include: `accuracy` and `L2 distance`
+
+    References
+    ----------
+    .. [1] Y. Kwon,
+        #TODO,
+        arXiv.org, 2023. [Online]. Available: #TODO.
+
+    Parameters
+    ----------
+    num_models : int, optional
+        Number of models to bag/aggregate, by default 1000
+    proportion : float, optional
+        Proportion of data points in the in-bag sample.
+        sample_size = len(dataset) * proportion, by default 1.0
+    random_state : RandomState, optional
+        Random initial state, by default None
+    """
+
+    def __init__(
+        self,
+        num_models: int = 1000,
+        proportion: int = 1.0,
+        random_state: RandomState = None,
+    ):
+        self.num_models = num_models
+        self.proportion = proportion
+        self.random_state = check_random_state(random_state)
+
+    def input_data(
+        self,
+        x_train: torch.Tensor,
+        y_train: torch.Tensor,
+        x_valid: torch.Tensor,
+        y_valid: torch.Tensor,
+    ):
+        """Stores and transforms input data for Data Out-Of-Bag Evaluator
+
+        Parameters
+        ----------
+        x_train : torch.Tensor
+            Data covariates
+        y_train : torch.Tensor
+            Data labels
+        x_valid : torch.Tensor
+            Test+Held-out covariates
+        y_valid : torch.Tensor
+            Test+Held-out labels
+        """
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_valid = x_valid
+        self.y_valid = y_valid
+
+        self.num_points = len(x_train)
+        self.label_dim = 1 if self.y_train.dim() == 1 else self.y_train.size(dim=1)
+        self.max_samples = round(self.proportion * self.num_points)
+        return self
+
+    def train_data_values(self, *args, **kwargs):
+        """This implementation is closer to the original implementation, although
+        it doesn't generalize to regressions
+
+        """
+        self.oob_pred = torch.zeros((0, self.label_dim), requires_grad=False)
+        self.oob_indices = GroupingIndex(start=1)
+
+        for i in tqdm.tqdm(range(self.num_models)):
+            in_bag = np.random.randint(0, self.num_points, self.max_samples)
+            # out_bag is the indices where the bincount is zero.
+            out_bag = (np.bincount(in_bag, minlength=self.num_points) == 0).nonzero()[0]
+
+            curr_model = copy.deepcopy(self.pred_model)
+            curr_model.fit(
+                Subset(self.x_train, indices=in_bag),
+                Subset(self.y_train, indices=in_bag),
+                *args,
+                **kwargs
+            )
+
+            y_hat = curr_model.predict(Subset(self.x_train, indices=out_bag))
+
+            self.oob_pred = torch.cat((self.oob_pred, y_hat), dim=0)
+            self.oob_indices.add_indices(out_bag)
+
+        return self
+
+    def evaluate_data_values(self) -> np.ndarray:
+        self.data_values = np.zeros(self.num_points)
+
+        for i, indices in self.oob_indices.items():
+            self.data_values[i] = self.evaluate(
+                self.y_train[i].expand(len(indices)), self.oob_pred[indices]
+            )
+        return self.data_values
+
+
+class GroupingIndex(defaultdict[int, list[int]]):
+    """Modified defaultdict to facilitate a groupings values and the corresponding
+    position of insertion
+
+    Parameters
+    ----------
+    start : int, optional
+        Starting insertion position, increments after each insertion, by default 0
+    """
+
+    def __init__(self, start: int = 0):
+        super(GroupingIndex, self).__init__(list)
+        self.position = start
+
+    def add_indices(self, indices: list[int]):
+        for i in indices:
+            self.__getitem__(i).append(self.position)
+            self.position += 1
