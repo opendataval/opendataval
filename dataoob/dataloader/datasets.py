988e2cbe7c107c4accb5c674847bc6bc8d6bbc0b
kevinfjiang <kevinfjiang@yahoo.com>
2023-04-05 13:48:03 -0400

    Refactored register to be in a seperate file from datasets.

--- dataoob/dataloader/datasets.py
+++ dataoob/dataloader/datasets.py
@@ -7,7 +7,6 @@ Catalog of registered data sets that can be used with
 :py:class:`~dataoob.dataloader.loader.DataLoader`. Pass in the ``str`` name registering
 the data set to load the data set as needed.
 """
-
 import os
 
 import numpy as np
